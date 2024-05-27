e3d7a32abe4b817f36d1edde3d1474748df5f5b8
Yuldashali <nosirovyuldashali@gmail.com>
2024-05-27 18:18:28 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '2rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl': '4rem',
+      },
+    },
+
+     
+    extend: {
+      colors: {
+        bg: 'rgb(18, 45, 66)',
+        color:'rgb(243, 243, 243)'
+      },
+      fontFamily: {
+        proximanova:['Proxima Nova'],
+        Italic:['Italic'],
+        natosans:['Nato Sans'],
+        play:['Play']
+      }
+    }
+  }
+}
